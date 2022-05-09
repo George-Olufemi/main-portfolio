b174d1909ded170e6e48095d1464cdbd9ed983d6
George Olufemi <georgeoluolufemi@gmail.com>
2022-05-09 22:16:47 +0100

    Modified Animation on the Landing Page

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -41,7 +41,7 @@ function LandingPage() {
             <motion.span
               initial={{ opacity: 0, y: -500 }}
               animate={{ opacity: 1, y: 0}}
-              transition={{ delay: 2, duration: 10, type: 'spring', stiffness: 200 }}
+              transition={{ delay: 2.2, duration: 10, type: 'spring', stiffness: 200 }}
             >e</motion.span> 
             <motion.span
               initial={{ opacity: 0, y: -500 }}
@@ -71,42 +71,42 @@ function LandingPage() {
             <motion.span
               initial={{ opacity: 0, y: -500 }}
               animate={{ opacity: 1, y: 0}}
-              transition={{ delay: 3.5, duration: 10, type: 'spring', stiffness: 200 }}
+              transition={{ delay: 3.8, duration: 10, type: 'spring', stiffness: 200 }}
             >o</motion.span>
             <motion.span
               initial={{ opacity: 0, y: -500 }}
               animate={{ opacity: 1, y: 0}}
-              transition={{ delay: 3.7, duration: 10, type: 'spring', stiffness: 200 }}
+              transition={{ delay: 4.1, duration: 10, type: 'spring', stiffness: 200 }}
             >r</motion.span>
             <motion.span
               initial={{ opacity: 0, y: -500 }}
               animate={{ opacity: 1, y: 0}}
-              transition={{ delay: 3.9, duration: 10, type: 'spring', stiffness: 200 }}
+              transition={{ delay: 4.4, duration: 10, type: 'spring', stiffness: 200 }}
             >t</motion.span>
             <motion.span
               initial={{ opacity: 0, y: -500 }}
               animate={{ opacity: 1, y: 0}}
-              transition={{ delay: 4, duration: 10, type: 'spring', stiffness: 200 }}
+              transition={{ delay: 4.7, duration: 10, type: 'spring', stiffness: 200 }}
             >f</motion.span>
             <motion.span
               initial={{ opacity: 0, y: -500 }}
               animate={{ opacity: 1, y: 0}}
-              transition={{ delay: 4.3, duration: 10, type: 'spring', stiffness: 200 }}
+              transition={{ delay: 5, duration: 10, type: 'spring', stiffness: 200 }}
             >o</motion.span>
             <motion.span
               initial={{ opacity: 0, y: -500 }}
               animate={{ opacity: 1, y: 0}}
-              transition={{ delay: 4.6, duration: 10, type: 'spring', stiffness: 200 }}  
+              transition={{ delay: 5.4, duration: 10, type: 'spring', stiffness: 200 }}  
             >l</motion.span>
             <motion.span
               initial={{ opacity: 0, y: -500 }}
               animate={{ opacity: 1, y: 0}}
-              transition={{ delay: 4.8, duration: 10, type: 'spring', stiffness: 200 }}
+              transition={{ delay: 5.8, duration: 10, type: 'spring', stiffness: 200 }}
             >i</motion.span>
             <motion.span
               initial={{ opacity: 0, y: -500 }}
               animate={{ opacity: 1, y: 0}}
-              transition={{ delay: 5.2, duration: 10, type: 'spring', stiffness: 200 }}
+              transition={{ delay: 6, duration: 10, type: 'spring', stiffness: 200 }}
             >o.</motion.span></span></h1>
         </div>
         <div className="flex justify-center items-center">
@@ -115,7 +115,7 @@ function LandingPage() {
             <motion.button
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
-              transition={{ delay: 7.3, duration: 2 }}
+              transition={{ delay: 7.8, duration: 2 }}
               className="text-white text-center font-semibold text-2xl bg-blue-600 px-6 py-2 rounded-xl hover:scale-125 transition-all hover:bg-blue-500">Proceed</motion.button>
             </Wobble>
         </Link>
